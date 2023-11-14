b59e09791c3868f948fcbe9e70314a4c52547b5b
Daniel Pérez <118856521+extdperperm@users.noreply.github.com>
2023-11-14 16:22:43 +0000

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+	
+	$("#btn1").on("click",function() {
+	 	alert("Presionado botón Ejemplo 1");
+	 });
+	 
+     $("#btn2").on("click",function() {
+        alert("Presionado botón Ejemplo 2");
+    });
+
+})
